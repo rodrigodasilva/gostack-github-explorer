f911b639e4132a5724963732aaff562ef05db929
rodrigodasilva <ribeiro.rodrigodasilva>
2020-05-20 19:42:17 -0300

    Conectando a api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,72 +1,68 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`/repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/28466370?s=460&u=f0fe77aa0acfe857ce6a10c0e41da92088590071&v=4"
-            alt="Avatar"
-          />
+        {repositories.map((repository) => (
+          <a href="teste" key={repository.full_name}>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>rodrigo/ribeiro</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-              rem quos dicta sint sit quisquam doloribus natus ea illum
-            </p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={30} color="#a8a8b3" />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/28466370?s=460&u=f0fe77aa0acfe857ce6a10c0e41da92088590071&v=4"
-            alt="Avatar"
-          />
-
-          <div>
-            <strong>rodrigo/ribeiro</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-              rem quos dicta sint sit quisquam doloribus natus ea illum
-            </p>
-          </div>
-
-          <FiChevronRight size={30} color="#a8a8b3" />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/28466370?s=460&u=f0fe77aa0acfe857ce6a10c0e41da92088590071&v=4"
-            alt="Avatar"
-          />
-
-          <div>
-            <strong>rodrigo/ribeiro</strong>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta,
-              rem quos dicta sint sit quisquam doloribus natus ea illum
-            </p>
-          </div>
-
-          <FiChevronRight size={30} color="#a8a8b3" />
-        </a>
+            <FiChevronRight size={30} color="#a8a8b3" />
+          </a>
+        ))}
       </Repositories>
     </>
   );
