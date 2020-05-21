bc985289c07d862c9f947224b0d89146e9dbfed8
rodrigodasilva <ribeiro.rodrigodasilva>
2020-05-20 21:10:26 -0300

    Estilizando pagina de Detalhes

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +13,55 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository: {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github explorer" />
+
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img src="" alt="Avatar" />
+
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Descrição do repository</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1880</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1880</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1880</strong>
+            <span>Stars</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="#">
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+
+          <FiChevronRight size={30} color="#a8a8b3" />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
